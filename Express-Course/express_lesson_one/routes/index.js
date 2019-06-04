467405f567a90e237ad01c6e1c719d845b47deba
codefuldom <dominiquehallan@gmail.com>
2019-06-03 20:30:24 -0500

    :fireworks: working thru modules

--- Express-Course/express_lesson_one/routes/index.js
+++ Express-Course/express_lesson_one/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router()
 router.get('/', function(req, res, next) {
   res.render('index', {
     title: 'My Cats',
-    users: [{ name: 'Maggie' }, { name: 'Slyvester' }, { name: 'Killer-K' }]
+    cats: [{ name: 'Maggie' }, { name: 'Slyvester' }, { name: 'Killer-K' }]
   })
 })
 
