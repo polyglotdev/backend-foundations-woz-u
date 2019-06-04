8fd2d5dc13fe66009a25511a09f1c16f4d45bdb0
codefuldom <dominiquehallan@gmail.com>
2019-06-03 20:18:38 -0500

    :fireworks: working thru modules

--- Express-Course/express_lesson_one/routes/index.js
+++ Express-Course/express_lesson_one/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node.js/Express' })
+  res.render('index', { title: 'Node.js/Express', name: 'Dominique' })
 })
 
 module.exports = router
