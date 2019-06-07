bb1bafc2a265257c26d11d9abadd5d7ed297fc4c
codefuldom <dominiquehallan@gmail.com>
2019-06-06 20:24:42 -0500

    :fireworks: changed the readme and added lesson 2 files

--- Express-Course/express_lesson_two/routes/index.js
+++ Express-Course/express_lesson_two/routes/index.js
@@ -5,16 +5,25 @@ let animals = ['pig', 'goat', 'dog', 'cat']
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let queryAnimal = req.query.animal
-  if (animals.includes(queryAnimal)) {
-    res.send(`Yep we have a ${queryAnimal}`)
-  } else {
-    res.send(`Nope, no ${queryAnimal} here..`)
-  }
+  // let queryAnimal = req.query.animal
+  // if (animals.includes(queryAnimal)) {
+  //   res.send(`Yep we have a ${queryAnimal}`)
+  // } else {
+  //   res.send(`Nope, no ${queryAnimal} here..`)
+  // }
+  res.render('index')
 })
 
 router.post('/', (req, res) => {
-  res.send('POST!')
+  let bodyAnimal = req.body
+
+  if (animals.includes(bodyAnimal.animal)) {
+    res.send(`Already have a ${bodyAnimal.animal}, thanks.`)
+  } else {
+    animals.push(bodyAnimal.animal)
+    console.log(animals)
+    res.send(`${animals}`)
+  }
 })
 
 router.put('/', (req, res) => {
