9c39cc0c226eefa851ed8c2bffb4a244507f10a2
codefuldom <dominiquehallan@gmail.com>
2019-06-12 20:54:41 -0500

    :fireworks: added the connection function

--- Express-Course/express_lesson_four/routes/index.js
+++ Express-Course/express_lesson_four/routes/index.js
@@ -1,10 +1,23 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 var mysql = require('mysql')
 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Password1!',
+  database: 'sakila'
+})
+
+connection.connect(function(err) {
+  if (err) {
+    console.error(err.message)
+  }
+  console.log(`You are connected to the database sucka`)
+})
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+module.exports = router
