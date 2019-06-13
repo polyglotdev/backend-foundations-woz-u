b8c2c49efa63b1d66e92c169baf1bcdea3ddf90e
codefuldom <dominiquehallan@gmail.com>
2019-06-12 23:13:38 -0500

    :fireworks: completed challenge

--- Express-Course/express_lesson_four/routes/index.js
+++ Express-Course/express_lesson_four/routes/index.js
@@ -38,4 +38,70 @@ router.get('/actor/:id', function(req, res, next) {
   })
 })
 
+const actorList = `SELECT * FROM actor`
+
+router.get('/actor', (req, res, next) => {
+  connection.query(actorList, (err, result) => {
+    res.render('actor', { actors: result })
+  })
+})
+
+router.post('/actor', (req, res, next) => {
+  console.log(req.body)
+  const newActor = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name
+  }
+
+  const selectActor = `SELECT * FROM ACTOR WHERE first_name = '${newActor.first_name}' AND last_name = '${
+    newActor.last_name
+  }'`
+
+  connection.query(selectActor, (err, result) => {
+    if (err) {
+      console.log(err.message)
+    } else if (result.length > 0) {
+      res.send(`Actor already exsits!`)
+    } else {
+      let newActorQuery = `
+			INSERT INTO actor(first_name, last_name)
+			VALUES('${newActor.first_name}',
+			'${newActor.last_name}')
+			`
+
+      connection.query(newActorQuery, (err, insertResult) => {
+        if (err) {
+          res.render('error', { message: `Oops, something went wrong` })
+        } else {
+          res.redirect('/actor')
+        }
+      })
+    }
+  })
+})
+
+router.get('/film/:id', function(req, res, next) {
+  let filmId = parseInt(req.params.id)
+  let filmActorQuery = `
+    SELECT film.title, actor.first_name, actor.last_name
+    FROM film INNER JOIN film_actor ON film.film_id = film_actor.film_id
+    INNER JOIN actor ON film_actor.actor_id = actor.actor_id
+    WHERE film.film_id = ${filmId}
+  `
+
+  connection.query(filmActorQuery, function(err, result) {
+    if (err) {
+      res.render('error', { message: err.message })
+    } else {
+      console.log(result)
+      res.render('filmDetails', {
+        filmTitle: result[0].title,
+        films: result
+      })
+    }
+  })
+})
+
+module.exports = router
+
 module.exports = router
